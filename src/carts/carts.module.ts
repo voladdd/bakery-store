24ea72f6d6f401a6912c1e80789b43e0dc1d0f57
Vlad <79471942+voladdd@users.noreply.github.com>
2022-11-15 23:42:33 +0300

    removed carts controller, need to redo whole routes logic

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -5,12 +5,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { CartsService } from './carts.service';
 import { User } from 'src/users/users.model';
 import { CartProducts } from './cart-products.model';
-import { CartsController } from './carts.controller';
 
 @Module({
   imports: [SequelizeModule.forFeature([User, Cart, CartProducts, Product])],
   providers: [CartsService],
   exports: [CartsService],
-  controllers: [CartsController],
 })
 export class CartsModule {}
