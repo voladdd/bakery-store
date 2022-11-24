c8a5064ec0a681b6d6676920087d9f73988192bd
Vlad <79471942+voladdd@users.noreply.github.com>
2022-11-24 23:07:27 +0300

    adding client routes

---
+++ client/src/routes.ts
@@ -0,0 +1,54 @@
+import Admin from "./pages/Admin";
+import Auth from "./pages/Auth";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import Seller from "./pages/Seller";
+import Shop from "./pages/Shop";
+import {
+  ADMIN_ROUTE,
+  CART_ROUTE,
+  LOGIN_ROUTE,
+  PRODUCT_ROUTE,
+  REGISTRATION_ROUTE,
+  SELLER_ROUTE,
+  SHOP_ROUTE,
+} from "./utils/consts";
+
+interface IRoutes {
+  path: string;
+  Component: () => JSX.Element;
+}
+
+export const authRoutes: IRoutes[] = [
+  {
+    path: ADMIN_ROUTE,
+    Component: Admin,
+  },
+  {
+    path: SELLER_ROUTE,
+    Component: Seller,
+  },
+  {
+    path: CART_ROUTE,
+    Component: Cart,
+  },
+];
+
+export const publicRoutes: IRoutes[] = [
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: REGISTRATION_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: SHOP_ROUTE,
+    Component: Shop,
+  },
+  {
+    path: PRODUCT_ROUTE + "/:id",
+    Component: Product,
+  },
+];
---
+++ client/src/utils/consts.ts
@@ -0,0 +1,7 @@
+export const ADMIN_ROUTE = "/admin";
+export const SELLER_ROUTE = "/seller";
+export const LOGIN_ROUTE = "/login";
+export const REGISTRATION_ROUTE = "/registration";
+export const SHOP_ROUTE = "/shop";
+export const CART_ROUTE = "/cart";
+export const PRODUCT_ROUTE = "/product";
